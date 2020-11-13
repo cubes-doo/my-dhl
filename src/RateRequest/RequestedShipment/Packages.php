76c01d3a5421e29d8aed23389fced62b1e0455c1
ACvijic <aleksa.cvijic@cubes.rs>
2020-11-13 11:40:05 +0100

    fix(raterequest): missing packages class

---
+++ src/RateRequest/RequestedShipment/Packages.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Cubes\MyDhl\RateRequest\RequestedShipment;
+
+use Cubes\MyDhl\RateRequest\RequestedShipment\Packages\RequestedPackage;
+
+class Packages
+{
+    public $RequestedPackages;
+
+    public function __construct(RequestedPackage $RequestedPackages) 
+    {
+        $this->RequestedPackages = $RequestedPackages;
+    }
+}
