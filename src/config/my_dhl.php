2d8cef1489b10218691fe6cf7ed0334f5c9310ea
ACvijic <aleksa.cvijic@cubes.rs>
2020-11-13 14:37:44 +0100

    feat: config acc id

--- src/config/my_dhl.php
+++ src/config/my_dhl.php
@@ -4,4 +4,5 @@ return [
     'base_path' => env('MY_DHL_BASE_PATH', 'https://wsbexpress.dhl.com:443/sndpt/'),
     'username' => env('MY_DHL_USERNAME', ''),
     'password' => env('MY_DHL_PASSWORD', ''),
+    'account_id'  => env('MY_DHL_ACCOUNT_ID', ''),
 ];
